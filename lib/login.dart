13fe04a7b364809d3ff91a803b7e84c258407017
aleenakrish <aleenakrish59@gmail.com>
2024-11-22 12:01:09 +0530

    second commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:appwrite_userauth/appwriteservices.dart';
+import 'package:appwrite_userauth/home.dart';
+import 'package:appwrite_userauth/signup.dart';
 import 'package:flutter/material.dart';
 
 class LogInScreen extends StatefulWidget {
@@ -23,22 +25,44 @@ class _LogInScreenState extends State<LogInScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
- SizedBox(height: 50,),
-          Text("Log In",style: TextStyle(fontSize: 25,fontWeight:FontWeight.bold),),
-          SizedBox(height: 10,),
-          TextFormField(
-            controller: emailCrl,
-            decoration: InputDecoration(border: OutlineInputBorder(),label: Text("Email")),
-          ),
-           SizedBox(height: 10,),
-          TextFormField(
-            controller: passwordCrl,
-            obscureText: true,
-            decoration: InputDecoration(border: OutlineInputBorder(),label: Text("Password")),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+         SizedBox(height: 50,),
+            Text("Log In",style: TextStyle(fontSize: 25,fontWeight:FontWeight.bold),),
+            SizedBox(height: 10,),
+            TextFormField(
+              controller: emailCrl,
+              decoration: InputDecoration(border: OutlineInputBorder(),label: Text("Email")),
+            ),
+             SizedBox(height: 10,),
+            TextFormField(
+              controller: passwordCrl,
+              obscureText: true,
+              decoration: InputDecoration(border: OutlineInputBorder(),label: Text("Password")),
+            ),
+            SizedBox(height: 25,),
+            ElevatedButton(onPressed: ()async{
+              if (emailCrl.text.isNotEmpty&&passwordCrl.text.isNotEmpty){
+                try{
+                  await _appwriteServices.loginUser(emailCrl.text, passwordCrl.text);
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+                  emailCrl.clear();
+                  passwordCrl.clear();
+                }catch(e){
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("check and try again")));
+                }
+              }
+            }, child: Text("Log In")),
+            SizedBox(height: 30,),
+            GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUpScreen()));
+              },child: Text("Dont Have An Account?",style: TextStyle(color: Colors.red),),
+            )
+          ],
+        ),
       ),
     );
   }
